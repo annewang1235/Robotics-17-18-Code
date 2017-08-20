d5c972c86bede3dccbe1f40fd6931c1ac9d6a863
annewang1235 <annewang3535@gmail.com>
2017-08-20 15:44:37 -0700

    register code

--- ftc_app-master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDriveOpMode.java
+++ ftc_app-master/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDriveOpMode.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by Anne on 8/11/2017.
  */
-
+@TeleOp(name="Simple Drive", group="Training")
 public class SimpleDriveOpMode extends OpMode {
 
     DcMotor leftwheel;
